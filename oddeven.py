4395d1b516500e4a056f91b836eb3a53b4c811b9
Mansoor Kanorewala <95083910+MansoorKanorewala@users.noreply.github.com>
2023-01-23 10:20:55 +0530

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -1,9 +1,9 @@
 #num = int(input("Enter any number to test whether it is odd or even:"))
-#num = 3
-#if (num % 2) == 0:
- #   print ("The number is even")
+num = 3
+if (num % 2) == 0:
+   print ("The number is even")
 
-#else:
-#     print ("The provided number is odd")
-num = int(input("Enter any number to test whether it is odd or even:"))
+else:
+    print ("The provided number is odd")
+#num = int(input("Enter any number to test whether it is odd or even:"))
 print(num)
